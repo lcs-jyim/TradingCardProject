47b53d7b5962b4eb1e317cf1af3ed1ca41df1a04
Josh Yim <jyim@lcs.on.ca>
2024-01-21 19:37:31 -0500

    Initial Commit

---
+++ TradingCardProject/TradingCardProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardProjectApp.swift
+//  TradingCardProject
+//
+//  Created by junxi Yim on 2024/1/21.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
